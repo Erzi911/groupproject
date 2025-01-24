dba5dc10fba91226f3a9fed2acfd6355ac0989b1
Almukhammed77 <amantaialmukhammed@gmail.com>
2025-01-24 10:16:56 +0500

    Delete src directory

--- src/controllers/interfaces/IAppointmentController.java
+++
@@ -1,7 +0,0 @@
-package controllers.interfaces;
-
-public interface IAppointmentController {
-    String createAppointment(int doctorId, int patientId, String date);
-    String getAppointmentById(int id);
-    String getAllAppointments();
-}
--- src/controllers/interfaces/IDoctorController.java
+++
@@ -1,7 +0,0 @@
-package controllers.interfaces;
-
-public interface IDoctorController {
-    String createDoctor(String name, String specialty);
-    String getDoctorById(int id);
-    String getAllDoctors();
-}
